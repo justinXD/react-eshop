3079642777489bf6f334234ac1fab5ad370cebd8
Justin D <isaac_justin@hotmail.com>
2023-03-07 13:31:54 -0600

    pages, components, containers & styles

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../styles/global.css'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Layout from "../containers/Layout.jsx";
-import Login from "../containers/login.jsx";
-import RecoveryPassword from '../containers/RecoveryPassword.jsx';
-import Home from "../pages/Home.jsx";
-import NotFound from "../pages/NotFound.jsx";
+import Layout from '../containers/Layout';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import NotFound from '../pages/NotFound';
 
 const App = () => {
     return (
@@ -13,10 +19,16 @@ const App = () => {
             <Layout>
                 <Switch>
                     {/* en las nuevas versiones de react Switch se cambio a Routes y component a element */}
-                    <Route exact path="/" component={ Home } />
-                    <Route exact path="/login" component={ Login } />
-                    <Route exact path="/recovery-password" component={ RecoveryPassword } />
-                    <Route path={"*"} component={ NotFound } />
+                    <Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
                 </Switch>
             </Layout>
         </BrowserRouter>
