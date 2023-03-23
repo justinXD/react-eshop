d0dcb424addf31d36fb2e97ff336325af72451c3
justin <isaac_justin@hotmail.com>
2023-03-22 22:57:16 -0600

    syntax fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from "@components/Header.jsx";
-import ProductItem from '@containers/ProductList.jsx';
+import ProductList from '@containers/ProductList.jsx';
 
 const Home = () => {
     return (
         <>
-            <ProductItem />
+            <ProductList />
         </>
     );
 }
